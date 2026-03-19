0d05fd8e2ebbd578078b6ddb199993f94ef3b942
poonam <poonamp@mindfiresolutions.com>
2026-03-19 17:50:21 +0530

    feat(storybook): path fix

--- apps/storybook/src/templates/pages/form-pages/contact-form/contact-form.stories.tsx
+++ apps/storybook/src/templates/pages/form-pages/contact-form/contact-form.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { ContactForm } from "./index";
 
 const meta: Meta<typeof ContactForm> = {
-  title: "Templates/Forms/ContactForm",
+  title: "Templates/Pages/Forms/ContactForm",
   component: ContactForm,
   parameters: {
     layout: 'fullscreen',
