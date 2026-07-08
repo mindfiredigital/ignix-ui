cef5ec2989d854da0478551139a8cc8842de3e69
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-07-08 18:22:13 +0530

    fix(registry): fix popup position issue of date-picker

--- packages/registry/components/date-picker/index.tsx
+++ packages/registry/components/date-picker/index.tsx
@@ -636,10 +636,10 @@ const getInRangeStyle = (themeMode: ThemeMode, colorScheme: ColorScheme): string
  */
 const getPopupPositionClasses = (position: string): string => {
     const positions: Record<string, string> = {
-        'bottom-left': 'top-full left-0 mt-2',
-        'bottom-right': 'top-full right-0 mt-2',
-        'top-left': 'bottom-full left-0 mb-2',
-        'top-right': 'bottom-full right-0 mb-2',
+        'bottom-left': 'top-full right-0 mt-2',
+        'bottom-right': 'top-full left-0 mt-2',
+        'top-left': 'bottom-full right-0 mb-2',
+        'top-right': 'bottom-full left-0 mb-2',
         'left': 'right-full top-0 mr-2',
         'right': 'left-full top-0 ml-2',
     };
