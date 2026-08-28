f30c90686f87d39283b9475fa9d50f013bf4baa1
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-08-28 17:03:02 +0530

    feat(component): add ai-response-action component

--- apps/docs/sidebars.ts
+++ apps/docs/sidebars.ts
@@ -59,7 +59,7 @@ const sidebars: SidebarsConfig = {
             'components/ai-suggested-actions',
             'components/ai-chat',
             'components/ai-conversation-history',
-            'components/ai-code-block'
+            'components/ai-response-actions'
            
           ],
         },
